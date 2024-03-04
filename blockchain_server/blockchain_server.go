034cd38211732baa19af8f2e037cd28744f0407d
Thành Quý <49222110+thanhquy1105@users.noreply.github.com>
2024-03-05 00:00:42 +0700

    launching web server and creating blockchain api

---
+++ blockchain_server/blockchain_server.go
@@ -0,0 +1,57 @@
+package blockchainserver
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/thanhquy1105/simpleblockchain/blockchain"
+	"github.com/thanhquy1105/simpleblockchain/wallet"
+)
+
+var cache map[string]*blockchain.Blockchain = make(map[string]*blockchain.Blockchain)
+
+type BlockchainServer struct {
+	port uint16
+}
+
+func NewBlockchainServer(port uint16) *BlockchainServer {
+	return &BlockchainServer{port}
+}
+
+func (bcs *BlockchainServer) Port() uint16 {
+	return bcs.port
+}
+
+func (bcs *BlockchainServer) GetBlockchain() *blockchain.Blockchain {
+	bc, ok := cache["Blockchain"]
+	if !ok {
+		minersWallet := wallet.NewWallet()
+		bc = blockchain.NewBlockchain(minersWallet.BlockchainAddress(), bcs.Port())
+		cache["Blockchain"] = bc
+		log.Printf("private_key	%v", minersWallet.PrivateKeyStr())
+		log.Printf("public_key	%v", minersWallet.PublicKeyStr())
+		log.Printf("blockchain_address	%v", minersWallet.BlockchainAddress())
+	}
+	return bc
+}
+
+func (bcs *BlockchainServer) GetChain(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		{
+			w.Header().Add("Content-Type", "application/json")
+			bc := bcs.GetBlockchain()
+			m, _ := bc.MarshalJSON()
+			io.WriteString(w, string(m[:]))
+		}
+	default:
+		log.Println("ERROR: Invalid HTTP method")
+	}
+}
+
+func (bcs *BlockchainServer) Run() {
+	http.HandleFunc("/", bcs.GetChain)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(bcs.Port())), nil))
+}
--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 
-	"github.com/thanhquy1105/simpleblockchain/blockchain"
-	"github.com/thanhquy1105/simpleblockchain/wallet"
+	blockchainserver "github.com/thanhquy1105/simpleblockchain/blockchain_server"
 )
 
 func init() {
@@ -13,23 +12,28 @@ func init() {
 }
 
 func main() {
-	walletMiner := wallet.NewWallet()
-	walletA := wallet.NewWallet()
-	walletB := wallet.NewWallet()
 
-	t := wallet.NewTransaction(walletA.PrivateKey(), walletA.PublicKey(),
-		walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0)
+	port := flag.Uint("port", 5000, "TCP port Number for Blockchain Server ")
+	flag.Parse()
+	app := blockchainserver.NewBlockchainServer(uint16(*port))
+	app.Run()
+	// walletMiner := wallet.NewWallet()
+	// walletA := wallet.NewWallet()
+	// walletB := wallet.NewWallet()
 
-	blockchain := blockchain.NewBlockchain(walletMiner.BlockchainAddress())
-	isAdded := blockchain.AddTransaction(walletA.BlockchainAddress(),
-		walletB.BlockchainAddress(), 1.0, walletA.PublicKey(), t.GenerateSignature())
+	// t := wallet.NewTransaction(walletA.PrivateKey(), walletA.PublicKey(),
+	// 	walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0)
 
-	fmt.Println("Added? ", isAdded)
+	// blockchain := blockchain.NewBlockchain(walletMiner.BlockchainAddress())
+	// isAdded := blockchain.AddTransaction(walletA.BlockchainAddress(),
+	// 	walletB.BlockchainAddress(), 1.0, walletA.PublicKey(), t.GenerateSignature())
 
-	blockchain.Mining()
-	blockchain.Print()
+	// fmt.Println("Added? ", isAdded)
 
-	fmt.Printf("A %.1f\n", blockchain.CalculateTotalAmount(walletA.BlockchainAddress()))
-	fmt.Printf("B %.1f\n", blockchain.CalculateTotalAmount(walletB.BlockchainAddress()))
-	fmt.Printf("M %.1f\n", blockchain.CalculateTotalAmount(walletMiner.BlockchainAddress()))
+	// blockchain.Mining()
+	// blockchain.Print()
+
+	// fmt.Printf("A %.1f\n", blockchain.CalculateTotalAmount(walletA.BlockchainAddress()))
+	// fmt.Printf("B %.1f\n", blockchain.CalculateTotalAmount(walletB.BlockchainAddress()))
+	// fmt.Printf("M %.1f\n", blockchain.CalculateTotalAmount(walletMiner.BlockchainAddress()))
 }
